6d0a0f2629530d1fa5dd8dc941055b560fcd0503
Amaury Chamayou <amaury@xargs.fr>
2025-12-12 15:56:47 +0000

    lzma

--- conv/size.py
+++ conv/size.py
@@ -3,6 +3,7 @@
 from conv import convert
 import os
 import pathlib
+import lzma
 
 if __name__ == "__main__":
     samples_dir = pathlib.Path("samples")
@@ -20,8 +21,11 @@ if __name__ == "__main__":
                     file_size = file_path.stat().st_size
                     converted_size = len(converted)
                     converted_size_with_string_refs = len(converted_with_string_refs)
+                    spdx_json = file_path.read_text()
+                    lzma_compressed = lzma.compress(spdx_json.encode())
+                    lzma_size = len(lzma_compressed)
                     print(
-                        f"{str(file_path):<64}: JSON: {str(file_size):<8} CBOR: {str(converted_size):<8}  Ratio: {converted_size / file_size:.2f} CBOR (string refs): {str(converted_size_with_string_refs):<8}  Ratio: {converted_size_with_string_refs / file_size:.2f}"
+                        f"{str(file_path):<64}: JSON: {str(file_size):<8} CoSPDX: {str(converted_size):<8}  Ratio: {converted_size / file_size:.2f} CoSPDXsr: {converted_size_with_string_refs / file_size:.2f} LZMA: {lzma_size / file_size:.2f}"
                     )
                 except Exception as e:
                     print(f"Error processing {file_path}: {e}")
